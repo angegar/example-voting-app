d05ed0a3919bd7a54ae7f43a47e6a8f477dea205
lgil3 <laurent.gil@dxc.com>
2019-04-30 16:20:46 +0200

    Change labels

--- vote/app.py
+++ vote/app.py
@@ -7,8 +7,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "DXC")
-option_b = os.getenv('OPTION_B', "HPE")
+option_a = os.getenv('OPTION_A', "Thé")
+option_b = os.getenv('OPTION_B', "Café")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
